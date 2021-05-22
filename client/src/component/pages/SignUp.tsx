92109061ff5c3e8fd93ef9d77016dda7d1acb236
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-05-22 17:26:25 +0530

    adding types for meterial ui

---
+++ client/src/component/pages/SignUp.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./SignUp.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { FormControl } from "@material-ui/core";
+
+function SignUp() {
+  return (
+    <div className="signUp">
+      <div className="main">
+        <div className="form">
+          <h1>SignUp</h1>
+          <FormControl>
+            <TextField id="standard-basic" label="Email" type="text" />
+            <TextField id="standard-basic" label="Name" type="text" />
+            <TextField id="standard-basic" label="Password" type="password" />
+            <Button variant="outlined" color="primary">
+              Submit
+            </Button>
+          </FormControl>
+        </div>
+        <div className="svg">
+          <img
+            src="https://image.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg"
+            alt="svg"
+            draggable="false"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
