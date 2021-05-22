a32df20c84a5e268a756552ce7199b3c53e2ec22
Luis Ramirez <sdaewa22@gmail.com>
2021-05-22 17:38:45 +0200

    adding socket to server

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,12 @@ import { app } from "./src/app";
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
 server.listen(port, () =>
   console.log(`server is listening on http://localhost:${port}`)
